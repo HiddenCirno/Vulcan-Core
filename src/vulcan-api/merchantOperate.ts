292fb8d41b0ce85387d6a15959f3087f54d101ae
bac213 <1991664032@qq.com>
2023-03-22 17:16:21 +0800

    加了几个注释

--- src/vulcan-api/merchantOperate.ts
+++ src/vulcan-api/merchantOperate.ts
@@ -19,8 +19,8 @@ export class TraderAppMerchandise implements TraderAssort {
     public items: Item[];
     public barter_scheme: IBarterScheme[][];
     public loyal_level_items: number;
-
-    constructor(itemID: string, count: number, shoppingGoods: string, loyal_level: number, parentId: string = "hideout", slotId: string = "hideout", upd: object = { "StackObjectsCount": 20000 }) {
+    //itemID: 你要卖的东西的ID  ,count:交换品要的个数   ,shoppingGoods:交换品   ,loyal_level:解锁物品的商人等级
+    constructor(itemID: string, count: number = 1, shoppingGoods: string = "5449016a4bdc2d6f028b456f", loyal_level: number = 1, parentId: string = "hideout", slotId: string = "hideout", upd: object = { "StackObjectsCount": 20000 }) {
         const item: Item = {
             "_id": "_bac_" + itemID + "213",
             "_tpl": itemID,
@@ -34,28 +34,28 @@ export class TraderAppMerchandise implements TraderAssort {
             "count": count,
             "_tpl": shoppingGoods
         })
-        
+
         this.items = new Array();
         this.items.push(item)
         this.barter_scheme = barterScheme;
         this.loyal_level_items = loyal_level;
     }
-
+    //trader:商人ID
     appMerchandise(container: DependencyContainer, trader: string,) {
         const dbs = container.resolve<DatabaseServer>("DatabaseServer");
         const db = dbs.getTables();
-        for(const item of this.items){
+        for (const item of this.items) {
             db.traders[trader].assort.items.push(item);
         }
         db.traders[trader].assort.barter_scheme[this.items[0]._id] = this.barter_scheme;
         db.traders[trader].assort.loyal_level_items[this.items[0]._id] = this.loyal_level_items;
     }
-
-    appitemMods(modId: string,modslotLocation: string){
+    //modId:你要加的配件ID  parentId:装到哪里(ps:默认当前实例的)   modslotLocation:装到哪个配件槽
+    appitemMods(modId: string,parentId:string = this.items[0]._id ,modslotLocation: string) {
         const modItem: Item = {
             "_id": "_bac_" + modId + "213",
             "_tpl": modId,
-            "parentId": this.items[0]._id,
+            "parentId": parentId,
             "slotId": modslotLocation
         }
         this.items.push(modItem);
